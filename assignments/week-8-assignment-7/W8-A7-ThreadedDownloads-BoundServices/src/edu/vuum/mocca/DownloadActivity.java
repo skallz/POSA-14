f71f57c8f36be05956feeac6652d9515cd7e0961
Jacek Psiuk <skallz@gmail.com>
2014-07-04 21:05:54 +0200

    [week-8] implementation

--- assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -76,7 +76,7 @@ public class DownloadActivity extends DownloadBase {
                 // service parameter into an interface that can be
                 // used to make RPC calls to the Service.
 
-                mDownloadCall = null;
+                mDownloadCall = DownloadCall.Stub.asInterface(service);
             }
 
             /**
@@ -109,7 +109,7 @@ public class DownloadActivity extends DownloadBase {
                 // service parameter into an interface that can be
                 // used to make RPC calls to the Service.
 
-                mDownloadRequest = null;
+                mDownloadRequest = DownloadRequest.Stub.asInterface(service);
             }
 
             /**
@@ -145,15 +145,22 @@ public class DownloadActivity extends DownloadBase {
                 // sendPath().  Please use displayBitmap() defined in
                 // DownloadBase.
 
-                Runnable displayRunnable = null;
+                Runnable displayRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        displayBitmap(imagePathname);
+                    }
+                };
+                runOnUiThread(displayRunnable);
             }
         };
      
     /**
      * This method is called when a user presses a button (see
      * res/layout/activity_download.xml)
+     * @throws RemoteException 
      */
-    public void runService(View view) {
+    public void runService(View view) throws RemoteException {
         Uri uri = Uri.parse(getUrlString());
 
         hideKeyboard();
@@ -162,12 +169,15 @@ public class DownloadActivity extends DownloadBase {
         case R.id.bound_sync_button:
             // TODO - You fill in here to use mDownloadCall to
             // download the image & then display it.
+            String image = mDownloadCall.downloadImage(uri);
+            displayBitmap(image);
             break;
 
         case R.id.bound_async_button:
             // TODO - You fill in here to call downloadImage() on
             // mDownloadRequest, passing in the appropriate Uri and
             // callback.
+            mDownloadRequest.downloadImage(uri, mDownloadCallback);
             break;
         }
     }
